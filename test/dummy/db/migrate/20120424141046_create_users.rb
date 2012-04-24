af9f3cf3ea0726baea83b0f772ffde4b2c90aa36
Robert Starsi <klevo@klevo.sk>
2012-04-24 16:14:03 +0200

    Tests pass

---
+++ test/dummy/db/migrate/20120424141046_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.string :auth_token
+      t.string :reset_password_token
+      t.datetime :password_reset_sent_at
+
+      t.timestamps
+    end
+  end
+end
