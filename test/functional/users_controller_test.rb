544e71c58a19958d70987d2cc8cdf234dbfd62e8
Robert Starsi <klevo@klevo.sk>
2012-04-24 16:40:47 +0200

    failing: "edit is not publicly accessible"

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -2,50 +2,15 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   fixtures :users
-  
-  setup do
-    @user = users(:one)
-  end
 
-  test "should get index" do
+  test "index is publicly accesible" do
     get :index
     assert_response :success
     assert_not_nil assigns(:users)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create user" do
-    assert_difference('User.count') do
-      post :create, user: { auth_token: @user.auth_token, email: @user.email, first_name: @user.first_name, last_name: @user.last_name, password_digest: @user.password_digest, password_reset_sent_at: @user.password_reset_sent_at, reset_password_token: @user.reset_password_token }
-    end
-
-    assert_redirected_to user_path(assigns(:user))
-  end
-
-  test "should show user" do
-    get :show, id: @user
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @user
-    assert_response :success
-  end
-
-  test "should update user" do
-    put :update, id: @user, user: { auth_token: @user.auth_token, email: @user.email, first_name: @user.first_name, last_name: @user.last_name, password_digest: @user.password_digest, password_reset_sent_at: @user.password_reset_sent_at, reset_password_token: @user.reset_password_token }
-    assert_redirected_to user_path(assigns(:user))
-  end
-
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete :destroy, id: @user
-    end
-
-    assert_redirected_to users_path
+  test "edit is not publicly accessible" do
+    get :edit, id: users(:robert)
+    assert_response :unauthorized
   end
 end
