b1f08215d3f703ea99a7118f43f92f47fc7ea990
Robert Starsi <klevo@klevo.sk>
2012-04-24 16:20:01 +0200

    fast commit

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -13,18 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20120424141046) do
 
-  create_table "authorizable_users", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.string   "auth_token"
-    t.string   "reset_password_token"
-    t.datetime "password_reset_sent_at"
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
