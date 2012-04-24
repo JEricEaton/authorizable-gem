cab6e8b75dbbb5c934a1aa5e8c78f31613708cbe
Robert Starsi <klevo@klevo.sk>
2012-04-24 19:01:06 +0200

    test "controller responds to current_user"

--- test/unit/authentication_test.rb
+++ test/unit/authentication_test.rb
@@ -2,9 +2,21 @@
 require 'test_helper'
 
 module Authorizable
+  class ExampleController < ActionController::Base
+    include Authorizable::Authentication
+  end
+  
   class AuthenticationTest < ActiveSupport::TestCase
     test "module" do
       assert Authentication.is_a?(Module)
+    end
+    
+    def setup
+      @controller = ExampleController.new
+    end
+    
+    test "controller responds to current_user" do
+      assert @controller.respond_to?(:current_user)
     end
   end
 end
