bc9c08bd0f85b22125c35c66cad48cb0b39b5bfd
Robert Starsi <klevo@klevo.sk>
2012-04-25 17:37:16 +0200

    test "sign in, sign out"

--- test/integration/session_flows_test.rb
+++ test/integration/session_flows_test.rb
@@ -4,7 +4,7 @@ class SessionFlowsTest < ActionDispatch::IntegrationTest
   include Capybara::DSL
   fixtures :users
   
-  test "sign in" do
+  test "sign in, sign out" do
     # Sign in
     visit '/sign_in'
     fill_in 'Email', :with => 'klevo@klevo.sk'
@@ -12,5 +12,9 @@ class SessionFlowsTest < ActionDispatch::IntegrationTest
     click_button 'Sign in'
     
     assert_equal user_url(users(:robert)), current_url
+    
+    # Sign out
+    click_on 'Sign out'
+    assert_equal sign_in_url, current_url
   end
 end
