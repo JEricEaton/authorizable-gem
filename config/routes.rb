933cfba3875e071c3ccdeaed06844f569ca80429
Robert Starsi <klevo@klevo.sk>
2012-04-25 16:38:19 +0200

    Routes config does not contain namespace

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@
 Rails.application.routes.draw do
   resource :sessions, :only => [:new, :create, :destroy]
 
-  # match 'sign_up'  => 'authorizable/users#new', :as => 'sign_up'
-  match 'sign_in'  => 'authorizable/sessions#new', :as => 'sign_in'
-  match 'sign_out' => 'authorizable/sessions#destroy', :via => :delete, :as => 'sign_out'
+  # match 'sign_up'  => 'users#new', :as => 'sign_up'
+  match 'sign_in'  => 'sessions#new', :as => 'sign_in'
+  match 'sign_out' => 'sessions#destroy', :via => :delete, :as => 'sign_out'
 end
 
