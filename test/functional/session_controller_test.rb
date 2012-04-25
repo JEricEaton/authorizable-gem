81926d0907feb20685dd29eac093f3272dfd1cdb
Robert Starsi <klevo@klevo.sk>
2012-04-25 13:17:40 +0200

    fast commit

--- test/functional/session_controller_test.rb
+++ test/functional/session_controller_test.rb
@@ -1,15 +1,21 @@
 require 'test_helper'
 
-class Authorizable::SessionsControllerTest < ActionController::TestCase
-  fixtures :users
-
-  test "new/sign in is publicly accessible" do
-    get :new
-    assert_response :success
+module Authorizable
+  class ::User
+    include Authorizable::User
   end
+
+  class SessionsControllerTest < ActionController::TestCase
+    fixtures :users
+
+    test "new/sign in is publicly accessible" do
+      get :new
+      assert_response :success
+    end
   
-  # test "succesful sign in" do
-  #   post :create, session: { email: 'klevo@klevo.sk', password: 'antonio' }
-  #   
-  # end
+    # test "succesful sign in" do
+    #   post :create, session: { email: 'klevo@klevo.sk', password: 'antonio' }
+    #   
+    # end
+  end
 end
