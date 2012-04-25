29e3b88a651459a277ccf55e0d1cdbae39fa5d0c
Robert Starsi <klevo@klevo.sk>
2012-04-25 15:01:23 +0200

    Sessions controller is not namespaced

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
-Authorizable::Engine.routes.draw do
-  resource :sessions,
-    :controller => 'authorizable/sessions',
-    :only       => [:new, :create, :destroy]
-
-  # match 'sign_up'  => 'authorizable/users#new', :as => 'sign_up'
-  match 'sign_in'  => 'authorizable/sessions#new', :as => 'sign_in'
-  match 'sign_out' => 'authorizable/sessions#destroy', :via => :delete, :as => 'sign_out'
-end
+# Authorizable::Engine.routes.draw do
+#   resource :sessions, :only => [:new, :create, :destroy]
+# 
+#   # match 'sign_up'  => 'authorizable/users#new', :as => 'sign_up'
+#   match 'sign_in'  => 'authorizable/sessions#new', :as => 'sign_in'
+#   match 'sign_out' => 'authorizable/sessions#destroy', :via => :delete, :as => 'sign_out'
+# end
 
 Rails.application.routes.draw do
-  resource :sessions,
-    :controller => 'authorizable/sessions',
-    :only       => [:new, :create, :destroy]
+  resource :sessions, :only => [:new, :create, :destroy]
 
   # match 'sign_up'  => 'authorizable/users#new', :as => 'sign_up'
   match 'sign_in'  => 'authorizable/sessions#new', :as => 'sign_in'
--- test/functional/session_controller_test.rb
+++ test/functional/session_controller_test.rb
@@ -1,44 +1,43 @@
 require 'test_helper'
 
-module Authorizable
-  class ::User
-    include Authorizable::User
+class ::User
+  include Authorizable::User
+end
+
+class SessionsControllerTest < ActionController::TestCase
+  fixtures :users
+  
+  def setup
+    Authorizable.configuration.password_salt = "$2a$10$fREDiaGGPkyyXBNXM/Ae/O"
+    @robert = users(:robert)
   end
 
-  class SessionsControllerTest < ActionController::TestCase
-    fixtures :users
-    
-    def setup
-      Authorizable.configuration.password_salt = "$2a$10$fREDiaGGPkyyXBNXM/Ae/O"
-      @robert = users(:robert)
-    end
+  test "new/sign in is publicly accessible" do
+    get :new
+    assert_response :success
+  end
 
-    test "new/sign in is publicly accessible" do
-      get :new
-      assert_response :success
-    end
+  test "succesful sign in with email and password" do
+    post :create, session: { email: 'klevo@klevo.sk', password: 'antonio' }
+    assert_redirected_to '/users/' + @robert.to_param # Defined in dummy's ApplicationController#redirect_to_after_sign_in
+    
+    @robert.reload
+    assert_not_equal @robert.auth_token, "RobertsAuthToken", 'auth_token has been regenerated'
+  end
   
-    test "succesful sign in with email and password" do
-      post :create, session: { email: 'klevo@klevo.sk', password: 'antonio' }
-      assert_redirected_to '/users/' + @robert.to_param # Defined in dummy's ApplicationController#redirect_to_after_sign_in
-      
-      @robert.reload
-      assert_not_equal @robert.auth_token, "RobertsAuthToken", 'auth_token has been regenerated'
-    end
-    
-    test "failed sign in" do
-      post :create, session: { email: 'klevo@klevo.sk', password: 'invalid' }
-      assert_response :success
-      assert_equal @robert.auth_token, "RobertsAuthToken", 'auth_token has not been regenerated'
-      assert_match /invalid/i, flash[:alert]
-    end
-    
-    test "sign out - resets auth_token field and removes cookie" do
-      @request.cookies[:auth_token] = 'RobertsAuthToken'
-      delete :destroy
-      @robert.reload
-      assert_equal @robert.auth_token, '', 'auth_token has been emptied'
-      assert_equal "auth_token=; path=/; expires=Thu, 01-Jan-1970 00:00:00 GMT", @response.header['Set-Cookie'], 'Remember me cookie gets deleted'
-    end
+  test "failed sign in" do
+    post :create, session: { email: 'klevo@klevo.sk', password: 'invalid' }
+    assert_response :success
+    assert_equal @robert.auth_token, "RobertsAuthToken", 'auth_token has not been regenerated'
+    assert_match /invalid/i, flash[:alert]
+  end
+  
+  test "sign out - resets auth_token field and removes cookie" do
+    @request.cookies[:auth_token] = 'RobertsAuthToken'
+    delete :destroy
+    @robert.reload
+    assert_equal @robert.auth_token, '', 'auth_token has been emptied'
+    assert_equal "auth_token=; path=/; expires=Thu, 01-Jan-1970 00:00:00 GMT", @response.header['Set-Cookie'], 'Remember me cookie gets deleted'
   end
 end
+
