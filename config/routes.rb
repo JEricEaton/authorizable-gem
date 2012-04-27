d2c92d6b43c8301117e01ea99991f0760f3a8c4c
Robert Starsi <klevo@klevo.sk>
2012-04-27 09:24:26 +0200

    fast commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
-# Authorizable::Engine.routes.draw do
-#   resource :sessions, :only => [:new, :create, :destroy]
-# 
-#   # match 'sign_up'  => 'authorizable/users#new', :as => 'sign_up'
-#   match 'sign_in'  => 'authorizable/sessions#new', :as => 'sign_in'
-#   match 'sign_out' => 'authorizable/sessions#destroy', :via => :delete, :as => 'sign_out'
-# end
-
 Rails.application.routes.draw do
   resource :sessions, :only => [:new, :create, :destroy]
   resource :password_resets
@@ -14,4 +6,3 @@ Rails.application.routes.draw do
   match 'sign_in'  => 'sessions#new', :as => 'sign_in'
   match 'sign_out' => 'sessions#destroy', :via => :delete, :as => 'sign_out'
 end
-
