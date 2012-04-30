e3b0072c89ced48c441e03fbba555aeb94a9036c
Robert Starsi <klevo@klevo.sk>
2012-04-30 14:48:49 +0200

    fast commit

--- lib/authorizable.rb
+++ lib/authorizable.rb
@@ -6,7 +6,6 @@ require "authorizable/user"
 require "authorizable/validators/email_format_validator"
 require "authorizable/validators/current_password_matches_validator"
 require "authorizable/password_strategies/bcrypt_hash_secret_strategy"
-require "authorizable/password_strategies/salted_sha1_strategy"
 
 Authorizable.configure do |config|
 end
