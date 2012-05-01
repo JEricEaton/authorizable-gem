d02217f3d3ff47c2968237ca1664dd4a5e6b4970
Robert Starsi <klevo@klevo.sk>
2012-05-01 10:56:55 +0200

    If user model responds to inactive? to such a check on sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,12 +10,18 @@ class SessionsController < ApplicationController
     reset_session # session fixation attack mitigation
     @user = Authorizable.configuration.user_model.find_by_email(session_params[:email])
     if @user.try(:authenticate, session_params[:password])
+      # :TODO test inactive user
+      if @user.respond_to?(:inactive?) && @user.inactive?
+        flash.now.alert = "Your account is inactive. Please find the email sent to you on sign up and follow the instructions."
+        render "new" and return
+      end
       @user.regenerate_auth_token
       if session_params[:remember_me].to_i
         cookies.permanent[:auth_token] = @user.auth_token
       else  
         cookies[:auth_token] = @user.auth_token
       end
+      # :TODO test this callback
       after_sign_in if respond_to?(:after_sign_in)
       redirect_to redirect_to_after_sign_in
     else
