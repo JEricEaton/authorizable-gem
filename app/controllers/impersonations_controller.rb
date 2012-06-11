74f318c45e4463e295c3b894c1e7c9be1380450e
Robert Starsi <klevo@klevo.sk>
2012-06-11 15:30:44 +0200

    After impersonation stop callback receives the impersonated user

--- app/controllers/impersonations_controller.rb
+++ app/controllers/impersonations_controller.rb
@@ -1,4 +1,6 @@
 class ImpersonationsController < ApplicationController
+  unloadable
+  
   def create
     raise Authorizable::NonAdminNotAllowedToImpersonateError unless current_user.admin?
     
@@ -12,11 +14,12 @@ class ImpersonationsController < ApplicationController
   end
   
   def stop
+    impersonated_user = @current_user
     session.delete :impersonated_user_id
     reload_current_user
     
     if respond_to?(:after_impersonation_stop)
-      send(:after_impersonation_stop)
+      send(:after_impersonation_stop, impersonated_user)
     else
       redirect_to root_path
     end
