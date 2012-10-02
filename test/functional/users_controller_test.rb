b0be4a4e43a0cf2274e03c14e743ba320ba17f4e
Robert Starsi <klevo@klevo.sk>
2012-10-02 12:48:01 +0200

    tests pass

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -3,23 +3,10 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
   fixtures :users
 
-  test "index is publicly accesible" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
-  end
-
   test "edit is not publicly accessible, should give unauthorized status code and render the unauthorized template" do
-    get :edit, id: users(:robert)
+    get :edit, id: users(:robert).to_param
     assert_response :redirect
-    assert_select 'h1', "You're not signed in"
-  end
-  
-  test "template in views/application should override the default template provided by the engine" do
-    Authorizable.configuration.unauthorized_template = 'custom_unauthorized'
-    get :edit, id: users(:robert)
-    assert_response :redirect
-    assert_select 'h1', "You're not allowed here dude!"
+    assert_redirected_to sign_in_path(r: edit_user_path(users(:robert).to_param))
   end
   
   test "blank remember me cookie does not authorize anyone" do
