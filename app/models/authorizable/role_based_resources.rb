36b09f94ea5fe71cedced4fa671c6bfe4bd4aec7
Robert Starsi <klevo@klevo>
2012-10-29 11:27:28 +0200

    Cool role based resources container class

--- app/models/authorizable/role_based_resources.rb
+++ app/models/authorizable/role_based_resources.rb
@@ -1,5 +1,37 @@
 module Authorizable
   class RoleBasedResources
-    
+    def initialize
+      @map = {}
+    end
+
+    def allow role, controller_with_actions
+      unless @map.has_key? role
+        @map[role] = []
+      end
+      @map[role] << controller_with_actions
+    end
+
+    def can_access? role, controller, action = nil
+      return true if role == :admin # admin can access everything
+      return false unless @map.has_key? role
+
+      resources = @map[role]
+      matches = 0
+      if action.nil?
+        matches = resources.select{ |item| item.to_s == controller.to_s }.count
+      else
+        matches = resources.select do |item| 
+          if item == controller
+            true
+          elsif item.respond_to?(:keys)
+            if item.keys == [controller]
+              allowed_actions = item.values.first
+              allowed_actions.include? action
+            end
+          end
+        end.count
+      end
+      matches > 0
+    end
   end
 end
--- test/unit/role_based_resources_test.rb
+++ test/unit/role_based_resources_test.rb
@@ -5,6 +5,22 @@ module Authorizable
   class RoleBasedResourcesTest < ActiveSupport::TestCase
     test "usage" do
       assert defined?(RoleBasedResources)
+
+      resources = RoleBasedResources.new
+      resources.allow :public, 'products'
+      resources.allow :public, 'users' => %w(index) 
+      resources.allow :product_manager, 'admin/products' => %w(index show)
+
+      assert resources.can_access?(:public, 'products')
+      assert resources.can_access?(:public, 'products', 'index')
+      refute resources.can_access?(:public, 'users')
+      assert resources.can_access?(:public, 'users', 'index')
+      refute resources.can_access?(:public, 'users', 'show')
+      refute resources.can_access?(:product_manager, 'admin/products')
+      assert resources.can_access?(:product_manager, 'admin/products', 'index')
+      assert resources.can_access?(:admin, 'products')
+      assert resources.can_access?(:admin, 'admin/products')
+      assert resources.can_access?(:admin, 'admin/products', 'show')
     end
   end
 end
