65c238db53f6ebe09c33e720bba1cf5423f1581c
Robert Starsi <klevo@klevo>
2012-10-29 10:52:14 +0200

    test "without product_manager or admin permissions you can not access product_managers resource"

--- test/integration/resources_for_role_test.rb
+++ test/integration/resources_for_role_test.rb
@@ -17,18 +17,9 @@ class ResourcesForRoleTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   
-  # test "without product_manager permissions you can not access product_manager's resource" do
-  #   path = '/admin/products'
-  #   # Sign in
-  #   visit '/sign_in'
-  #   fill_in 'Email', :with => 'klevo@klevo.sk'
-  #   fill_in 'Password', :with => 'antonio'
-  #   click_button 'Sign in'
-    
-  #   assert_equal user_url(@robert), current_url
-    
-  #   # Sign out
-  #   click_on 'Sign out'
-  #   assert_equal sign_in_url, current_url
-  # end
+  test "without product_manager or admin permissions you can not access product_managers resource" do
+    cookies[:auth_token] = @andrea.auth_token
+    get '/admin/products'
+    assert_response :redirect
+  end
 end
