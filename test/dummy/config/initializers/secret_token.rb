217c2b461b8ba45100cc5d6dedeb367ca9d16173
lesliepoolman <leslie@equallife.org>
2021-02-13 10:47:27 -0500

    Bundle update, remove unused files

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'c482734768ff03d53b410818c57bf7588a157e379ad0b528680c029f9cb52974ee3c62d494961d9b11345028c61d0d88afe26eccd2d82b2b158386fbee28ab05'
-Dummy::Application.config.secret_key_base = '818c50705e43e35e221efe661881de91e03a7cda4e33a0f4b8802f733de6ce41e97c36dc25191f3e1a3d7363e3b0266890ce017578af56e0f3cae0299fa3cae1'
