9c8ce419f7f92bc3fcafd846737349818ea4144c
Leslie Poolman <leslie.john.poolman01@gmail.com>
2026-05-21 16:09:22 -0500

    Rebuild password reset token to use Rails design pulled from rails 8 as rails 7.2 does not include this by default

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,14 +1,11 @@
 class PasswordResetsController < ApplicationController
   skip_before_action :require_authentication
+  before_action :set_user, only: %i[update edit]
 
   def new; end
 
   def update
-    set_user
-    @user.force_password_validation = true
-    if @user.password_reset_sent_at.blank? || @user.password_reset_expired?
-      redirect_to new_password_reset_path, alert: 'Password reset has expired, you need to try again.'
-    elsif @user.update(user_params)
+    if @user.update(user_params)
       redirect_to sign_in_path, notice: 'Password has been reset. You can sign in using your new password.'
     else
       render :edit
@@ -19,15 +16,18 @@ class PasswordResetsController < ApplicationController
     email = params[:email].to_s.chomp.strip
     raise ActiveRecord::RecordNotFound if email.blank? || email.size < 3
 
-    user = User.where(email:).first
-    PasswordResetsMailer.reset(user).deliver_now if user.try(:create_password_reset_token)
-    redirect_to new_password_reset_path,
-                notice: 'Email sent with password reset instructions. Please check your email inbox.'
+    user = User.find_by(email: email)
+    if user
+      user.update(password_reset_token: user.password_reset_token)
+      PasswordResetsMailer.reset(user).deliver_now
+      redirect_to new_password_reset_path,
+                  notice: 'Email sent with password reset instructions. Please check your email inbox.'
+    else
+      redirect_to new_password_reset_path, alert: 'Email address not found, please try again.'
+    end
   end
 
-  def edit
-    set_user
-  end
+  def edit; end
 
   private
 
@@ -36,11 +36,10 @@ class PasswordResetsController < ApplicationController
   end
 
   def set_user
-    # Do not allow to fuck around with anything else than at least a 6 characters long string
     token = params[:id].to_s.chomp.strip
-    raise ActiveRecord::RecordNotFound if token.blank? || token.size < 6
-
-    @user = User.where(Authorizable.configuration.password_reset_token_column_name => token).first
-    raise ActiveRecord::RecordNotFound unless @user
+    @user = User.find_by_password_reset_token!(token)
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to new_password_reset_path, alert: 'Invalid Reset token, please try again!'
+    return
   end
 end
--- app/mailers/password_resets_mailer.rb
+++ app/mailers/password_resets_mailer.rb
@@ -1,6 +1,7 @@
 class PasswordResetsMailer < ApplicationMailer
   def reset(user)
     @user = user
+    @token = user.password_reset_token
     mail_to_user_with_subject 'Password Reset Instructions'
   end
 end
