09bf12af24b46f0db955706875250e329aa91876
Leslie Poolman <leslie.john.poolman01@gmail.com>
2026-05-21 16:11:50 -0500

    rebuild user impersonations using encrypted cookie and user id

--- test/functional/impersonations_controller_test.rb
+++ test/functional/impersonations_controller_test.rb
@@ -4,19 +4,18 @@ class ImpersonationsControllerTest < ActionController::TestCase
   fixtures :users
 
   def setup
-    Authorizable.configuration.password_salt = "$2a$10$fREDiaGGPkyyXBNXM/Ae/O"
     @robert = users(:robert)
     @andrea = users(:andrea)
     @my_cookies = ActionDispatch::Request.new(Rails.application.env_config.deep_dup).cookie_jar
   end
 
   test "create - Robert (admin) starts impersonating Andrea" do
-    @my_cookies.encrypted[:auth_token] = 'RobertsAuthToken' # auth as Robert
-    cookies[:auth_token] = @my_cookies[:auth_token]
+    @my_cookies.encrypted[:user] = @robert.id # auth as Robert
+    cookies[:user] = @my_cookies[:user]
 
     assert_nil session[:impersonated_user_id]
 
-    post :create, params: { user_id: @andrea.id}
+    post :create, params: { user_id: @andrea.id }
     assert_response :redirect
     assert_redirected_to root_path
     assert_equal @andrea.id, session[:impersonated_user_id]
@@ -24,23 +23,23 @@ class ImpersonationsControllerTest < ActionController::TestCase
   end
 
   test "anonymous user is not allowed to impersonate" do
-    assert_nil cookies.encrypted[:auth_token]
-    post :create, params: { user_id: @andrea.id}
+    assert_nil cookies.encrypted[:user]
+    post :create, params: { user_id: @andrea.id }
     assert_response :redirect
   end
 
   test "non admin is not allowed to impersonate" do
-    @my_cookies.encrypted[:auth_token] = 'AndreasAuthToken' # auth as Andrea
-    cookies[:auth_token] = @my_cookies[:auth_token]
+    @my_cookies.encrypted[:user] = @andrea.id # auth as Andrea
+    cookies[:user] = @my_cookies[:user]
 
     assert_raise Authorizable::NonAdminNotAllowedToImpersonateError do
-      post :create, params: { user_id: @robert.id}
+      post :create, params: { user_id: @robert.id }
     end
   end
 
   test "stop impersonation" do
-    @my_cookies.encrypted[:auth_token] = 'RobertsAuthToken' # auth as Robert
-    cookies[:auth_token] = @my_cookies[:auth_token]
+    @my_cookies.encrypted[:user] = @robert.id # auth as Robert
+    cookies[:user] = @my_cookies[:user]
     session[:impersonated_user_id] = @andrea.id
     post :stop
     assert_nil session[:impersonated_user_id]
