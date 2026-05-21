b5b20c1fc2bcff662661bd1ee66e039372b3a3e2
Leslie Poolman <leslie.john.poolman01@gmail.com>
2026-05-21 16:14:07 -0500

    rebuild testing to test against new has secure password usage

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,13 +1,8 @@
-# encoding: UTF-8
 require 'test_helper'
 
 module Authorizable
   class UserTest < ActiveSupport::TestCase
     fixtures :users
-
-    def setup
-      Authorizable.configuration.password_salt = "$2a$10$fREDiaGGPkyyXBNXM/Ae/O"
-    end
 
     test "module" do
       assert Authorizable::User.is_a?(Module)
@@ -26,53 +21,23 @@ module Authorizable
       assert !robert.authenticate('notthere')
     end
 
-    test "password_reset_expired?" do
-      user = users(:robert)
-      user.password_reset_sent_at = Time.mktime(2011, 1, 2, 10, 1)
-
-      travel_to Time.mktime(2011, 1, 2, 12, 0) do
-        assert !user.password_reset_expired?
-      end
-
-      travel_to Time.mktime(2011, 1, 2, 12, 2) do
-        assert user.password_reset_expired?
-      end
-    end
-
-    test "currrent password validation needs to be explicitly enabled to check for current password" do
+    test "with current password validation no valid does not pass" do
       robert = users(:robert)
-      invalid_password_update_params = { password: 'newPassword', password_confirmation: 'newPassword', current_password: '' }
-      assert robert.update(invalid_password_update_params)
-      assert robert.authenticate('newPassword')
-    end
 
-    test "with enabled current password validation empty or no valud does not pass" do
-      robert = users(:robert)
-      robert.validate_current_password = true
-
-      invalid_password_update_params = { password: 'newPassword2', password_confirmation: 'newPassword2', current_password: '' }
+      invalid_password_update_params = { password: 'newPassword23', password_challenge: '' }
       assert !robert.update(invalid_password_update_params)
-      assert !robert.authenticate('newPassword2')
+      assert !robert.reload.authenticate('newPassword23')
 
-      invalid_password_update_params = { password: 'newPassword2', password_confirmation: 'newPassword2' }
+      invalid_password_update_params = { password: 'newPassword2', password_challenge: 'invalid' }
       assert !robert.update(invalid_password_update_params)
-      assert !robert.authenticate('newPassword2')
-
-      invalid_password_update_params = { password: 'newPassword2', password_confirmation: 'newPassword2', current_password: nil }
-      assert !robert.update(invalid_password_update_params)
-      assert !robert.authenticate('newPassword2')
-
-      invalid_password_update_params = { password: 'newPassword2', password_confirmation: 'newPassword2', current_password: 'invalid' }
-      assert !robert.update(invalid_password_update_params)
-      assert !robert.authenticate('newPassword2')
+      assert !robert.reload.authenticate('newPassword2')
     end
 
     test "valid password update" do
       robert = users(:robert)
-      robert.validate_current_password = true
 
-      valid_password_update_params = { password: 'newPassword2', password_confirmation: 'newPassword2', current_password: 'antonio' }
-      assert robert.update(valid_password_update_params)
+      valid_password_update_params = { password: 'newPassword2', password_challenge: 'antonio' }
+      assert robert.update!(valid_password_update_params)
       assert robert.authenticate('newPassword2')
     end
   end
