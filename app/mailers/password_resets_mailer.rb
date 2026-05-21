49e818076915f35c130248388a0be209c19aefbe
Leslie Poolman <leslie.john.poolman01@gmail.com>
2026-05-21 16:15:07 -0500

    ensure user password reset token is saved

--- app/mailers/password_resets_mailer.rb
+++ app/mailers/password_resets_mailer.rb
@@ -1,7 +1,6 @@
 class PasswordResetsMailer < ApplicationMailer
   def reset(user)
     @user = user
-    @token = user.password_reset_token
     mail_to_user_with_subject 'Password Reset Instructions'
   end
 end
